f3d278707e2f87303216d7dce5791087badd5aba
petrosTsatsis <it22016@hua.gr>
2023-11-14 17:37:44 +0200

    added week and month event display options and also method for sorting the event lists

--- src/main/java/gr/dit/hua/calendar/Calendar.java
+++ src/main/java/gr/dit/hua/calendar/Calendar.java
@@ -7,15 +7,12 @@ import biweekly.property.DateEnd;
 import biweekly.property.DateStart;
 import biweekly.property.Description;
 import biweekly.property.Summary;
-import biweekly.util.ICalDate;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,30 +20,37 @@ public class Calendar {
 
     private List<VEvent> events;
 
-    private String[] args;
+    private List<VEvent> sortedEvents;
 
     public Calendar(){}
 
-    public List<VEvent> getEvents() {
-        return events;
+    public void displayEvents(List<VEvent> events, String message){
+
+        System.out.println("\n" + message + "\n");
+
+        for (VEvent event : events) {
+
+            Summary summary = event.getSummary();
+            DateStart dtStart = event.getDateStart();
+            DateEnd dtEnd = event.getDateEnd();
+            Description description = event.getDescription();
+
+            System.out.println("--------------------");
+            System.out.println("Title: " + (summary != null ? summary.getValue() : ""));
+            System.out.println("Start Date: " + (dtStart != null ? dtStart.getValue() : ""));
+            System.out.println("End Date: " + (dtEnd != null ? dtEnd.getValue() : ""));
+            System.out.println("Description: " + (description != null ? description.getValue() : ""));
+            System.out.println("--------------------");
+        }
+
     }
 
-    public void setEvents(List<VEvent> events) {
-        this.events = events;
-    }
-
-    public String[] getArgs() {
-        return args;
-    }
-
-    public void setArgs(String[] args) {
-        this.args = args;
-    }
-
+    // view the events from the current day
     public void displayDayEvents(String filePath) throws IOException {
         String content = readFile(filePath);
         ICalendar ical = Biweekly.parse(content).first();
         LocalDateTime dateTimeNow = LocalDateTime.now();
+        events = new ArrayList<>();
 
         if (ical != null) {
             System.out.println("Events in the iCal file:");
@@ -55,23 +59,72 @@ public class Calendar {
                 LocalDateTime eventStartDateTime = convertDateStartToLocalDateTime(event.getDateStart());
 
                 if (eventStartDateTime.toLocalDate().isEqual(dateTimeNow.toLocalDate())) {
-                    Summary summary = event.getSummary();
-                    DateStart dtStart = event.getDateStart();
-                    DateEnd dtEnd = event.getDateEnd();
-                    Description description = event.getDescription();
-
-                    System.out.println("--------------------");
-                    System.out.println("Title: " + (summary != null ? summary.getValue() : ""));
-                    System.out.println("Start Date: " + (dtStart != null ? dtStart.getValue() : ""));
-                    System.out.println("End Date: " + (dtEnd != null ? dtEnd.getValue() : ""));
-                    System.out.println("Description: " + (description != null ? description.getValue() : ""));
-                    System.out.println("--------------------");
+                    events.add(event);
                 }
             }
+            sortedEvents = EventLists.sortByStartDate(events);
+
+            displayEvents(sortedEvents, "Today's events : ");
         } else {
             System.out.println("Invalid or empty iCal file.");
         }
     }
+
+    // view the events from the current day till the end of the week
+    public void displayWeekEvents(String filePath) throws IOException {
+        String content = readFile(filePath);
+        ICalendar ical = Biweekly.parse(content).first();
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        events = new ArrayList<>();
+
+        if (ical != null) {
+
+            LocalDateTime startOfWeek = dateTimeNow.with(dateTimeNow.getDayOfWeek()).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            LocalDateTime endOfWeek = dateTimeNow.with(DayOfWeek.SUNDAY).plusDays(1).withHour(23).withMinute(59).withSecond(59);
+
+            for (VEvent event : ical.getEvents()) {
+                LocalDateTime eventStartDateTime = convertDateStartToLocalDateTime(event.getDateStart());
+
+                if (eventStartDateTime.isAfter(startOfWeek) && eventStartDateTime.isBefore(endOfWeek) ) {
+                    events.add(event);
+                }
+            }
+            sortedEvents = EventLists.sortByStartDate(events);
+
+            displayEvents(sortedEvents, "Week's events :");
+        } else {
+            System.out.println("Invalid or empty iCal file.");
+        }
+
+    }
+
+    // view the events from the current month
+    public void displayMonthEvents(String filePath) throws IOException {
+        String content = readFile(filePath);
+        ICalendar ical = Biweekly.parse(content).first();
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        events = new ArrayList<>();
+
+
+        if (ical != null) {
+            LocalDateTime startOfMonth = dateTimeNow.withDayOfMonth(dateTimeNow.getDayOfMonth()).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            LocalDateTime endOfMonth = dateTimeNow.withDayOfMonth(dateTimeNow.getMonth().length(dateTimeNow.toLocalDate().isLeapYear())).withHour(23).withMinute(59).withSecond(59);
+
+            for (VEvent event : ical.getEvents()) {
+                LocalDateTime eventStartDateTime = convertDateStartToLocalDateTime(event.getDateStart());
+
+                if (eventStartDateTime.isAfter(startOfMonth) && eventStartDateTime.isBefore(endOfMonth) ) {
+                    events.add(event);
+                }
+            }
+            sortedEvents = EventLists.sortByStartDate(events);
+
+            displayEvents(sortedEvents, "Month's events :");
+        }else {
+            System.out.println("Invalid or empty iCal file.");
+        }
+    }
+
     // read the ical file
     private static String readFile(String filePath) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(filePath));
@@ -79,7 +132,7 @@ public class Calendar {
     }
 
     // change the format from DateStart to LocalDateTime
-    private static LocalDateTime convertDateStartToLocalDateTime(DateStart dateStart) {
+    static LocalDateTime convertDateStartToLocalDateTime(DateStart dateStart) {
         Date date = dateStart.getValue();
         return LocalDateTime.ofInstant(date.toInstant(), java.time.ZoneId.systemDefault());
     }
--- src/main/java/gr/dit/hua/calendar/Main.java
+++ src/main/java/gr/dit/hua/calendar/Main.java
@@ -1,13 +1,12 @@
 package gr.dit.hua.calendar;
 
-import biweekly.component.VEvent;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         Calendar calendar = new Calendar();
+
         // check length of the given arguments
         if(args.length > 2) {
             System.out.println("\nPlease do not select more than two arguments.");
@@ -15,9 +14,33 @@ public class Main {
             // if the arguments are two go on with the options
             if (args.length == 2 && args[1].endsWith(".ics")){
                 // if the first argument is "day", call the method to display the events of the current day
-                if(args[0].matches("day")){
-                    calendar.displayDayEvents(args[1]);
+
+                switch (args[0]){
+                    case "day" :
+                        calendar.displayDayEvents(args[1]);
+                        break;
+
+                    case "week" :
+                        calendar.displayWeekEvents(args[1]);
+                        break;
+
+                    case "month" :
+                        calendar.displayMonthEvents(args[1]);
+                        break;
+
+                    default:
+                        System.out.println("Invalid input. Please try one of the following as the first argument if you want to view events:");
+                        System.out.println("  - day");
+                        System.out.println("  - week");
+                        System.out.println("  - month");
+                        System.out.println("  - pastday");
+                        System.out.println("  - pastweek");
+                        System.out.println("  - pastmonth");
+                        System.out.println("  - todo");
+                        System.out.println("  - due");
+                        break;
                 }
+
 
             }
         }
